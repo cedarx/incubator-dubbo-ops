59df478791699b42b5a6513ca2e4e6af2fa6cd1e
cedars <cedars@MBP>
2018-04-12 14:46:21 +0800

    add icon

--- dubbo-admin/src/main/java/com/alibaba/dubbo/governance/biz/common/i18n/MessageResourceService.java
+++ dubbo-admin/src/main/java/com/alibaba/dubbo/governance/biz/common/i18n/MessageResourceService.java
@@ -18,8 +18,8 @@ package com.alibaba.dubbo.governance.biz.common.i18n;
 
 public interface MessageResourceService {
 
-    public String get(String key, Object... args);
+    String get(String key, Object... args);
 
-    public String getMessage(String key, Object... args);
+    String getMessage(String key, Object... args);
 
 }
